1c4b314f7c82c2148e7049eb53297ba49cd7847e
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2013-12-26 13:21:58 -0800

    Set up usage block.

--- R_function_graph.py
+++ R_function_graph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 
-import io, re, getopt
+import io, sys, re, getopt, os
 
 '''
 This script will scrape all function calls from an R package and create a matrix
@@ -8,3 +8,46 @@ that can be used with the igraph R package to plot a graph of all the functions.
 
 Yes, igraph is available for python, but I am not as familiar with it. 
 '''
+
+def usage():
+	print("\n%s Copyright (C) Zhian N. Kamvar 2013" % sys.argv[0])
+	print("Software comes with no warranty.")
+	print("Usage:\n\tpython %s -d <path/to/R-package>")
+	print("Options:")
+	print("\t-h:\tShow help and quit.")
+	print("\t-v:\tVerbose.")
+	print("\t-c:\ttraverse /src directory for C functions")
+	print("")
+	sys.exit(2)
+
+
+
+
+if __name__ == '__main__':
+
+	verbose = False
+	cfunk = False
+	package_directory = ""
+	myopts, args = getopt.getopt(sys.argv[1:], "hvcd:", ["directory="])
+
+	if len(myopts) < 1:
+		usage()
+
+	for opt, arg in myopts:
+		if opt == "-h":
+			usage()
+		elif opt == "-v":
+			verbose = True
+		elif opt == "-c":
+			cfunk = True
+		elif opt in ("-d", "--directory"):
+			package_directory = arg
+		else:
+			usage()
+
+	print("Verbose: "+str(verbose))
+	print("C Funk: "+str(cfunk))
+	print("R Package Directory: "+ os.path.abspath(package_directory))
+
+
+
